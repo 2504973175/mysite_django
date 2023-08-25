919acaffabc7227aedf6fd7cd24d56745545d04d
yangyali@uino.com <yangyali@uino.com>
2023-08-25 18:29:01 +0800

    django添加自动化测试

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,9 @@
+import datetime
 from django.db import models
 
 # Create your models here.
 from django.db import models
-
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -10,7 +11,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
